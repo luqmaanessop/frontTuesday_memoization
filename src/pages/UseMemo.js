0bf0ab408233fbe881ca61053ae1c16eebbb98c9
Luqmaan Essop <luqmaan.essop@amazee.com>
2022-09-05 16:51:40 +0200

    Add usememo tut rework

--- src/pages/UseMemo.js
+++ src/pages/UseMemo.js
@@ -6,11 +6,13 @@ import ChildCounter from '../components/ChildCounter'
 export default function UseMemo() {
   const [localNumber, setLocalNumber] = useState(0)
   const [childNumber, setChildNumber] = useState(0)
+  // This is where you would use useMemo, to memoize expensives calculations
   const [arr, setArr] = useState([1,2,3,4,5,6,7,8,9,10])
 
   const memoizedCallback = useCallback(number => changeChildNumber(number), [])
+  // 💰 Uncomment out the memoizedLargestValue and use it instead of the largestUnmemoizedValue
   // const memoizedLargestValue = useMemo(() => getLargestNumber(), [arr])
-const largestUnmemoizedValue = getLargestNumber();
+  const largestUnmemoizedValue = getLargestNumber();
 
   function incrememntLocal() {
     setLocalNumber(state => state + 1)
@@ -21,6 +23,7 @@ const largestUnmemoizedValue = getLargestNumber();
     setChildNumber(number)
   }
 
+  // This is super expensive, maybe its a fetch request
   function getLargestNumber() {
     console.log("Calculating largest number - this is so expensive");
     return Math.max(...arr);
