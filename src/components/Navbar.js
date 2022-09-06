19768c7fa1be6d565a3086f507b99f65f9f5dbd3
Luqmaan Essop <luqmaan.essop@amazee.com>
2022-09-06 08:57:51 +0200

    Bring tailwind + style nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,18 +4,18 @@ import { Link } from "react-router-dom";
 function Navbar() {
   return (
     <nav>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
+      <ul class="flex p-6 bg-blue-800">
+        <li class="mr-6">
+          <Link class="text-white hover:text-blue-100" to="/">Home</Link>
         </li>
-        <li>
-          <Link to="/memo">React.memo Demo</Link>
+        <li class="mr-6">
+          <Link class="text-white hover:text-blue-100" to="/memo">React.memo Demo</Link>
         </li>
-        <li>
-          <Link to="/usecallback">useCallback Demo</Link>
+        <li class="mr-6">
+          <Link class="text-white hover:text-blue-100" to="/usecallback">useCallback Demo</Link>
         </li>
-        <li>
-          <Link to="/usememo">useMemo Demo</Link>
+        <li class="mr-6">
+          <Link class="text-white hover:text-blue-100" to="/usememo">useMemo Demo</Link>
         </li>
       </ul>
     </nav>
