d5f5568beec9054adc9b14ddcbbd22d79f4a2c45
Luqmaan Essop <luqmaan.essop@amazee.com>
2022-09-06 09:08:36 +0200

    Style rest of pages

--- src/components/ChildCounter.js
+++ src/components/ChildCounter.js
@@ -6,9 +6,9 @@ const ChildCounter = (props) => {
   }
 
   return (
-    <div>
-      <h1>Child Counter: {props.number}</h1>
-      <button onClick={changeNumber}>Click to randomize the child number</button>
+    <div class="p-6">
+      <h1 class="text-xl mb-1">Child Counter: {props.number}</h1>
+      <button class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow" onClick={changeNumber}>Click to randomize the child number</button>
     </div>
   )
 }
--- src/pages/Memo.js
+++ src/pages/Memo.js
@@ -15,8 +15,8 @@ export default function Memo() {
     <>
     <Navbar />
     <ChildCounter number={childNumber}/>
-    <button onClick={incrememntLocal}>Click to increment local</button>
-    <h1>Local Counter: {localNumber}</h1>
+    <button class="mt-4 ml-6 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full" onClick={incrememntLocal}>Click to increment local</button>
+    <h1 class="text-xl pl-6">Local Counter: {localNumber}</h1>
     </>
   )
 }
